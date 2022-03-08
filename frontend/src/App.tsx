3d669b2275e867af1f7d59ce68f3da206241f62d
Guilherme Deitos <guilherme.cascavel@gmail.com>
2022-03-07 22:09:57 -0300

    Componente Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
+import Navbar from './components/navbar/index'
 
 function App() {
   return (
-    <h1>Página principal</h1>
+    <Navbar/>
   );
 }
 
---
+++ frontend/src/components/navbar/index.tsx
@@ -0,0 +1,24 @@
+import{ReactComponent as GithubIcon} from '../../assets/img/github.svg'
+
+import './styles.css'
+
+function navbar(){
+    return (
+        <header>
+          <nav className="container">
+            <div className="dsmovie-nav-content">
+              <h1>DSMovie</h1>  
+              <a href="https://github.com/GuilhermeDeitos"> 
+                <div className="dsmovie-contact-container">
+                  <GithubIcon/>
+                  <p className="dsmovie-contact-link">/GuilhermeDeitos</p>
+                </div>
+              </a>
+            </div>  
+    
+          </nav>
+        </header>
+      );
+}
+
+export default navbar
