553af5e40b9742e482dde4f030488f735653e88b
Guilherme Deitos <guilherme.cascavel@gmail.com>
2022-03-07 22:23:10 -0300

    Rotas do react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,28 @@
-import Navbar from './components/navbar/index'
+/* eslint-disable react/jsx-no-comment-textnodes */
+//Rotas
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+//Componentes de paginas
+import Listing from './pages/listing';
+import Form from './pages/form';
+//Componentes de blocos de paginas
+import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <Navbar/>
+    //iniciar configuração das rotas
+    <BrowserRouter> 
+      <Navbar />
+      <Routes> /* Configuração rota por rota */
+        <Route path="/" element={<Listing />} /> /*Pagina  principal*/
+        <Route path="/form"> /* Rota dos formulários com a subrota que possuí o id do filme */
+          <Route path=":movieId" element={<Form />} /> 
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
